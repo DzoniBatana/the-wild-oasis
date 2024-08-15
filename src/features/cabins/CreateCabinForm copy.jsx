1d67644f27a5474ac1fd6f739fc879abd8a877be
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2024-08-15 20:46:02 +0200

    Update cabinForm

--- src/features/cabins/CreateCabinForm copy.jsx
+++ src/features/cabins/CreateCabinForm copy.jsx
@@ -10,13 +10,8 @@ import Textarea from "../../ui/Textarea";
 import FormRow from "../../ui/FormRow";
 import toast from "react-hot-toast";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
-  const { id: editId, ...editValues } = cabinToEdit;
-  const isEditSession = Boolean(editId);
-
-  const { register, handleSubmit, reset, getValues, formState } = useForm({
-    defaultValues: isEditSession ? editValues : {},
-  });
+function CreateCabinForm() {
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
   const { errors } = formState;
   console.log(errors);
 
@@ -129,7 +124,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
           id="image"
           accept="image/*"
           {...register("image", {
-            required: isEditSession ? false : "This field is required",
+            required: "This field is required",
           })}
         />
       </FormRow>
@@ -139,9 +134,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button disabled={isCreating}>
-          {isEditSession ? "Edit cabin" : "Create new cabin"}
-        </Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
