b479468882bee3c99550f44bd1003769972ba8e4
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-19 23:05:47 +0100

    Uploading Modal

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -11,7 +11,7 @@ import FormRow from "../../ui/FormRow";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { isCreating, createCabin } = useCreateCabin();
   const { isEditing, editCabin } = useEditCabin();
   const isWorking = isCreating || isEditing;
@@ -35,6 +35,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
           // eslint-disable-next-line
           onSuccess: (data) => {
             reset();
+            onCloseModal?.();
           },
         }
       );
@@ -45,6 +46,7 @@ function CreateCabinForm({ cabinToEdit = {} }) {
           // eslint-disable-next-line
           onSuccess: (data) => {
             reset();
+            onCloseModal?.();
           },
         }
       );
@@ -55,7 +57,10 @@ function CreateCabinForm({ cabinToEdit = {} }) {
   }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+    <Form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      type={onCloseModal ? "modal" : "regular"}
+    >
       <FormRow label="Cabin name" error={errors?.name?.message}>
         <Input
           type="text"
@@ -139,7 +144,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disabled={isWorking}>
