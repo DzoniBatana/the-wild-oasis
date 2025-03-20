3bdcb156dc153a36fc7cd884a75b33df5233cda4
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-20 21:51:58 +0100

    Update

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,3 +1,4 @@
+import { createContext, useContext } from "react";
 import styled from "styled-components";
 
 const StyledTable = styled.div`
@@ -57,3 +58,39 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContaxt = createContext();
+
+function Table({ columns, children }) {
+  return (
+    <TableContaxt.Provider value={{ columns }}>
+      <StyledTable role="table">{children}</StyledTable>
+    </TableContaxt.Provider>
+  );
+}
+
+function Header({ children }) {
+  const { columns } = useContext(TableContaxt);
+  return (
+    <StyledHeader role="row" columns={columns} as="header">
+      {children}
+    </StyledHeader>
+  );
+}
+function Row({ children }) {
+  const { columns } = useContext(TableContaxt);
+  return (
+    <StyledRow role="row" columns={columns}>
+      {children}
+    </StyledRow>
+  );
+}
+
+function Body({ children }) {}
+
+Table.Header = Header;
+Table.Body = Body;
+Table.Row = Row;
+Table.Footer = Footer;
+
+export default Table;
