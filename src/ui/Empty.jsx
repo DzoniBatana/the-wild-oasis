eb4aa59e105ebac2c2f5a92b13028640c9c7542a
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-25 22:39:09 +0100

    Update

--- src/ui/Empty.jsx
+++ src/ui/Empty.jsx
@@ -1,5 +1,6 @@
-function Empty({ resource }) {
-  return <p>No {resource} could be found.</p>;
+// eslint-disable-next-line
+function Empty({ resourceName }) {
+  return <p>No {resourceName} could be found.</p>;
 }
 
 export default Empty;
