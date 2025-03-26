be163775f21c063f7b2a31014b183ab586fcc6e5
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-26 21:56:57 +0100

    Filter Bookings update

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,15 +1,17 @@
+import BookingTableOperations from "../features/bookings/BookingTableOperations";
+import BookingTable from "../features/bookings/BookingTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-//import BookingTable from "../features/bookings/BookingTable";
 
 function Bookings() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
+        <BookingTableOperations />
       </Row>
 
-      {/* <BookingTable /> */}
+      <BookingTable />
     </>
   );
 }
