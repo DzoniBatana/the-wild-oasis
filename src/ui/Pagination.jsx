410129503489801fe0cd2d235d8365d40797dd96
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-28 21:39:20 +0100

    API side pagination update

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -2,6 +2,7 @@ import { HiChevronLeft } from "react-icons/hi";
 import { HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
 import styled from "styled-components";
+import { PAGE_SIZE } from "./../utils/constans";
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -58,8 +59,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const { searchParams, setSearchParams } = useSearchParams();
