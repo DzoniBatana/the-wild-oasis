57dff2a54ba614a18ce8efe0daa9bebe565e2d48
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-03-31 22:40:47 +0200

    Update login form

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,18 +1,20 @@
+/* eslint-disable */
 import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
-import { login } from "../../services/apiAuth";
+import SpinnerMini from "../../ui/SpinnerMini";
+import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("nikola@gmail.com ");
+  const [password, setPassword] = useState("nikola997");
+  const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!email || !password) return;
-
     login({ email, password });
   }
 
@@ -26,6 +28,7 @@ function LoginForm() {
           autoComplete="username"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow label="Password" orientation="vertical">
@@ -35,10 +38,13 @@ function LoginForm() {
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRow>
       <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
+        <Button size="large" disabled={isLoading}>
+          {!isLoading ? "Log in " : <SpinnerMini />}
+        </Button>
       </FormRow>
     </Form>
   );
