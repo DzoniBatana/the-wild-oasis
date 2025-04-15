14e02706972c4e7e5c199364aef6afb48206805e
Nikola <66076379+DzoniBatana@users.noreply.github.com>
2025-04-15 22:51:00 +0200

    Error update resolving (react-error-boundery)

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable */
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +33,24 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundery }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">
+            Something went wrong, yoy can try again later... 🧐
+          </Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundery}>
+            Try again please!
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+export default ErrorFallback;
